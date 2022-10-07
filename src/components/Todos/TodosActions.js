0d1950e11a7f6abf0eea170e5ee93745d8437c96
Nikolay Oreschenko <nickolas1594@mail.ru>
2022-10-07 17:21:10 +0300

    25-todo-app-v2-pre-relize

---
+++ src/components/Todos/TodosActions.js
@@ -0,0 +1,25 @@
+import { RiDeleteBin2Line, RiRefreshLine } from 'react-icons/ri'
+
+import Button from '../UI/Button'
+/* .. выйти из папки Todos в папку components 
+и лальше просто в папки зайти */
+
+import styles from './TodosActions.module.css'
+
+function TodosActions({ resetTodos, deleteCompletedTodos }) {
+  /* деструктуризаия на месте */
+  return (
+    <div className={styles.todosActionsContainer}>
+    
+      <Button title="Reseset Todos" onClick={resetTodos}>
+        <RiRefreshLine />
+      </Button>
+
+      <Button title="Clear Completed Todos" onClick={deleteCompletedTodos}>
+        <RiDeleteBin2Line />
+      </Button>
+    </div>
+  )
+}
+
+export default TodosActions
